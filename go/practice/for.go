8e9adc7502c745535087ccc999800453d4cec564
Yuki Hirai <yukihirai0505@gmail.com>
2017-05-13 13:11:13 +0900

    Update for.go

--- go/practice/for.go
+++ go/practice/for.go
@@ -12,8 +12,15 @@ func PrintFor() {
 
 func PrintForContinued() {
 	sum := 1
+	// it is like while statement
 	for sum < 1000 {
 		sum += sum
 	}
 	fmt.Println(sum)
+}
+
+func forever() {
+	// if there is no condition, it is infinite loop.
+	for {
+	}
 }
