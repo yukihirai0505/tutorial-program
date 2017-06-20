7b85992cd1d93d78d42946714a0e8723853b94ca
Yuki Hirai <yukihirai0505@gmail.com>
2017-06-20 13:11:11 +0900

    Update Square to functional components

--- react/tutorial/src/index.js
+++ react/tutorial/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -21,6 +19,8 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
+    // immutability is important
+    // => https://facebook.github.io/react/tutorial/tutorial.html#why-immutability-is-important
     const squares = this.state.squares.slice();
     squares[i] = 'X';
     this.setState({squares: squares})
