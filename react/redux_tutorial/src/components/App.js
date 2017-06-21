fa3b6cea6b08b20eb4ec90b1bfac2d1fc8cac5b6
Yuki Hirai <yukihirai0505@gmail.com>
2017-06-21 15:09:14 +0900

    Add base todo app

--- react/redux_tutorial/src/components/App.js
+++ react/redux_tutorial/src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div>
-    Hello World!
+    <VisibleTodoList/>
   </div>
 )
 
--- react/redux_tutorial/src/index.js
+++ react/redux_tutorial/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todo from './reducers'
 import App from './components/App'
+import { addTodo } from './actions'
+
+let store = createStore(todo)
+
+store.dispatch(addTodo('Hello World!'))
+store.dispatch(addTodo('Hello Redux!'))
+console.log(store.getState())
 
 render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
