d45d1d4931de46ec89f367f4e83106fbca6da867
Yuki Hirai <yukihirai0505@gmail.com>
2017-06-21 17:52:55 +0900

    Add filterlink

--- react/redux_tutorial/src/components/Link.js
+++ react/redux_tutorial/src/components/Link.js
@@ -1,8 +1,19 @@
-import React, { PropTypes } from 'react'
+import React, {PropTypes} from 'react'
 
-const Link = ({ children, onClick }) => (
-  <a href="#">{children}</a>
-)
+const Link = ({active, children, onClick}) => {
+  if (active) {
+    return <span>{children}</span>
+  }
+
+  return (
+    <a href="#"
+       onClick={(e) => {
+         e.preventDefault()
+         onClick()
+       }}
+    >{children}</a>
+  )
+}
 
 Link.propTypes = {
   children: PropTypes.node.isRequired
