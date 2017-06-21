89bb3a43409b6714a1e0eede985101c54aa2da95
Yuki Hirai <yukihirai0505@gmail.com>
2017-06-21 17:13:25 +0900

    Add addTodoInput

--- react/redux_tutorial/src/components/App.js
+++ react/redux_tutorial/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div>
-    <VisibleTodoList/>
+    <AddTodo />
+    <VisibleTodoList />
   </div>
 )
 
