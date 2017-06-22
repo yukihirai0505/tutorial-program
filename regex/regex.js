77f588cfe17a1d60f7c712562fcf3a93bff7540f
Yuki Hirai <yukihirai0505@gmail.com>
2017-06-22 09:12:50 +0900

    Update regex

--- regex/regex.js
+++ regex/regex.js
@@ -36,3 +36,20 @@ var rs = s.match(/^@t.guchi.*l$/);
 if (rs) {
   console.log('マッチしました!')
 }
+
+// part4
+
+// {} => repeat num for before character
+// 0{2} => 00
+// @{2,} => 00,000,00000
+// @{2,4} => 00, 000, 0000
+
+// [a-z]{5}
+// [a-z]{3,5}
+
+// part5
+
+// a? => 0 or 1 =>  , a
+// a* => 0 or more => , a, aaaa, aaaaaa
+// a+ => 1 or more => a, aaa, aaaaaa
+
