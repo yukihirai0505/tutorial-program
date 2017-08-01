d737c829e93c90699c894d7fa4f988ab19fba756
Yuki Hirai <yukihirai0505@gmail.com>
2017-08-01 19:33:46 +0900

    Add comment

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    /*
+     * comment
+     */
+    std::cout << "Hello, World!" << std::endl; // comment
     return 0;
 }
