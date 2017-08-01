58375553ca4cac4e488f87951e63a81ac6a5e7dc
Yuki Hirai <yukihirai0505@gmail.com>
2017-08-01 19:36:55 +0900

    Add variable

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     /*
      * comment
      */
     std::cout << "Hello, World!" << std::endl; // comment
+    const int              a = 3;
+    const std::string      s = "abc";
+    const std::vector<int> v = {1, 2, 3};
+    std::cout << s << std::endl;
     return 0;
 }
