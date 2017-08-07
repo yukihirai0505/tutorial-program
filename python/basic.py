a88d970106f3cffb5782ed1208bca1941c7d9592
Yuki Hirai <yukihirai0505@gmail.com>
2017-08-07 22:19:16 +0900

    Add python basic code

---
+++ python/basic.py
@@ -0,0 +1,16 @@
+# calculate
+print(1-2)
+print(4*5)
+print(7/5)
+print(3**2)
+
+# check type
+print(type(10))
+print(type(2.78))
+print(type("hello"))
+
+# use variable
+x = 10
+print(x)
+x = 100
+print(x)
