d771a3f007ad809305e2d8f89606ad0a1c046496
Yuki Hirai <yukihirai0505@gmail.com>
2017-08-07 22:23:41 +0900

    Add python list code

--- python/basic.py
+++ python/basic.py
@@ -14,3 +14,20 @@ x = 10
 print(x)
 x = 100
 print(x)
+y = 3.14
+print(x*y)
+print(type(x*y))
+
+# list
+a = [1, 2, 3, 4, 5]
+print(a)
+print(len(a))
+print(a[0])
+print(a[4])
+a[4] = 99
+print(a)
+print(a[0:2])
+print(a[1:])
+print(a[:3])
+print(a[:-1])
+print(a[:-2])
