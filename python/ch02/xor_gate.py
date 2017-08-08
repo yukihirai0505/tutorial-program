d8968cd7697fd165194e6692e9322e7992564323
Yuki Hirai <yukihirai0505@gmail.com>
2017-08-08 15:50:53 +0900

    Add xor_gate

---
+++ python/ch02/xor_gate.py
@@ -0,0 +1,15 @@
+from ch02.and_gate02 import AND
+from ch02.nand_gate import NAND
+from ch02.or_gate import OR
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    return AND(s1, s2)
+
+
+if __name__ == '__main__':
+    for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+        y = XOR(xs[0], xs[1])
+        print(str(xs) + " -> " + str(y))
