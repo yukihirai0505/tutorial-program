d1ac8635844e3895f83cb590383a33cf2a4aec62
Yuki Hirai <yukihirai0505@gmail.com>
2017-08-08 15:15:35 +0900

    Add nand_gate

--- python/ch02/nand_gate.py
+++ python/ch02/nand_gate.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 
-def AND(x1, x2):
+def NAND(x1, x2):
     x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.7
+    w = np.array([-0.5, -0.5])
+    b = 0.7
     tmp = np.sum(w * x) + b
     if tmp <= 0:
         return 0
@@ -14,5 +14,5 @@ def AND(x1, x2):
 
 if __name__ == '__main__':
     for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
-        y = AND(xs[0], xs[1])
+        y = NAND(xs[0], xs[1])
         print(str(xs) + " -> " + str(y))
