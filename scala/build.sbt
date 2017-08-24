2b810ea2e80dbca40e12e6f62d07670dc03a3919
Yuki Hirai <yukihirai0505@gmail.com>
2017-08-24 18:23:33 +0900

    Add elastic4s example

--- scala/build.sbt
+++ scala/build.sbt
@@ -4,10 +4,20 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-stream" % "2.4.2",
-  "org.scalatest" %% "scalatest" % "2.2.4" % "test"
-)
+libraryDependencies ++= {
+  val akkaStreamV = "2.5.2"
+  Seq(
+    // Akka
+    "com.typesafe.akka" %% "akka-stream" % akkaStreamV,
+    // Elasticsearch
+    "org.apache.lucene" % "lucene-core" % "3.6.0",
+    "com.sksamuel.elastic4s" %% "elastic4s-http" % "5.4.2",
+    // Joda
+    "joda-time" % "joda-time" % "2.9.3",
+    // Test
+    "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+  )
+}
 
 // Uncomment to use Akka
 //libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
---
+++ scala/src/main/scala/com/example/HttpClientExampleApp.scala
@@ -0,0 +1,31 @@
+package com.example
+
+import com.sksamuel.elastic4s.ElasticsearchClientUri
+import com.sksamuel.elastic4s.http.HttpClient
+import com.sksamuel.elastic4s.http.search.SearchResponse
+import org.elasticsearch.action.support.WriteRequest.RefreshPolicy
+
+object HttpClientExampleApp extends App {
+
+  // you must import the DSL to use the syntax helpers
+  import com.sksamuel.elastic4s.http.ElasticDsl._
+
+  val client = HttpClient(ElasticsearchClientUri("localhost", 9200))
+
+  client.execute {
+    bulk(
+      indexInto("myindex" / "mytype").fields("country" -> "Mongolia", "capital" -> "Ulaanbaatar"),
+      indexInto("myindex" / "mytype").fields("country" -> "Namibia", "capital" -> "Windhoek")
+    ).refresh(RefreshPolicy.WAIT_UNTIL)
+  }.await
+
+  val result: SearchResponse = client.execute {
+    search("myindex").matchQuery("capital", "ulaanbaatar")
+  }.await
+
+  // prints out the original json
+  println(result.hits.hits.head.sourceAsString)
+
+  client.close()
+
+}
