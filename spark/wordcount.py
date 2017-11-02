5b8c2436a476030921701829bd8d14666a82caf7
Yuki Hirai <yukihirai0505@gmail.com>
2017-11-02 19:45:46 +0900

    Update wordcount

--- spark/wordcount.py
+++ spark/wordcount.py
@@ -3,20 +3,22 @@
 
 from pyspark import SparkContext
 
+
 def execute(sc, src, dest):
-    '''
+    """
     ワードカウントを実行する
-    '''
+    """
     # srcファイルを読み込み
     text_file = sc.textFile(src)
     counts = text_file.flatMap(lambda line: line.split(" ")) \
-                 .map(lambda word: (word, 1)) \
-                 .reduceByKey(lambda a, b: a + b)
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a + b)
     # 結果を書き出し
     counts.saveAsTextFile(dest)
 
+
 if __name__ == '__main__':
     sc = SparkContext('local', 'WordCount')
-    src  = './input'
+    src = './input'
     dest = './output'
     execute(sc, src, dest)
