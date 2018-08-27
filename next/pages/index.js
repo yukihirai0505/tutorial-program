836239342acda39f7251c07c9b4fa0bfec20070b
Yuki Hirai <yukihirai0505@gmail.com>
2018-08-27 10:54:38 +0900

    Add button instead of anchor link

--- next/pages/index.js
+++ next/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Index = () => (
   <div>
     <Link href="/about">
-      <a style={{fontSize: 20}}>About Page</a>
+      <button>Go to About Page</button>
     </Link>
     <p>Hello Next.js</p>
   </div>
