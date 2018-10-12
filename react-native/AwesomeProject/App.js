2116da5ff4484e1f9bad57595cdfcef006aef644
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-12 14:36:04 +0900

    We must first build an app that does nothing except say "Hello world"

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -20,10 +20,8 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        <Text>Hello world!</Text>
       </View>
     );
   }
