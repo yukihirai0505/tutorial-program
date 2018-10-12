e06b410960ae05e8cebb9c45efbb993c42cdc72a
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-12 16:33:58 +0900

    Add Button

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -5,39 +5,57 @@
  * @format
  * @flow
  */
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, TextInput} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image, TextInput, Button, Alert } from 'react-native';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
-  render() {
-    return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
-      </View>
-    );
-  }
+    _onPressButton() {
+        Alert.alert('You tapped the button!')
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Press Me"
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Press Me"
+                        color="#841584"
+                    />
+                </View>
+                <View style={styles.alternativeLayoutButtonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="This looks great!"
+                    />
+                    <Button
+                        onPress={this._onPressButton}
+                        title="OK!"
+                        color="#841584"
+                    />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-    textAlignVertical: "center",
-    textAlign: "center"
-  },
-  red: {
-    color: 'red',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        margin: 20
+    },
+    alternativeLayoutButtonContainer: {
+        margin: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
 });
