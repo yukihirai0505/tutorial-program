bd58f696b510d45b6e6c7a28d2e311deedb0b798
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-12 16:19:55 +0900

    Add text input sample

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -5,74 +5,26 @@
  * @format
  * @flow
  */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, TextInput} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-
-class Greeting extends Component {
-  render() {
-    return (
-      <Text>Hello {this.props.name}!</Text>
-    );
-  }
-}
-
-class Blink extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {isShowingText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
+    this.state = {text: ''};
   }
-
   render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
     return (
-      <Text>{display}</Text>
-    );
-  }
-}
-
-export default class App extends Component<Props> {
-  render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-    return (
-      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-between'}}>
-        {/* <Text>Hello world!</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-        <Blink text='I love to blink' />
-        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} /> */}
-          {/* <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-          <View style={{flex: 2, backgroundColor: 'skyblue'}}>
-            <Text style={styles.bigblue}>hoge</Text>
-          </View>
-          <View style={{flex: 3, backgroundColor: 'steelblue'}} /> */}
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-        </View>
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
     );
   }
 }
