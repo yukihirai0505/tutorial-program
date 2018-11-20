204483d3a1867e44290b57b2ff44e6a4b6bb1e64
Yuki Hirai <yukihirai0505@gmail.com>
2018-11-20 18:46:23 +0900

    Update image nearest neighbors

--- programming/python/opencv/image-nearest-neighbors/README.md
+++ programming/python/opencv/image-nearest-neighbors/README.md
@@ -9,4 +9,7 @@ $ conda install -c menpo opencv=3.4.1
 
 ## Reference
 
+- [OpenCV-Pythonチュートリアル](http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/index.html)
+- [OpenCV 3で犬と猫を分類できるように学習してみる（BOW: Bag Of Visual Words, KNN: k-Nearest Neighbour, k-meansクラスタリング, KAZE）](https://qiita.com/hitomatagi/items/883770046de5746a5deb)
 - [画像の特徴点を抽出する](https://qiita.com/fukuit/items/546f19d2abf98eccd3e7)
+- []()
--- programming/python/opencv/image-nearest-neighbors/main.py
+++ programming/python/opencv/image-nearest-neighbors/main.py
@@ -4,6 +4,7 @@ import cv2
 # 比較しまくって一番似ている画像を探す といったことができる distance でソートして 最初の distanceが一番近いもの
 a_img = cv2.imread('test_img/rabbit03.jpg')
 b_img = cv2.imread('test_img/rabbit01.jpg')
+# b_img = cv2.imread('test_img/rabbit01.jpg')
 
 # グレー画像に変換
 a_gray = cv2.cvtColor(a_img, cv2.COLOR_BGR2GRAY)
@@ -14,13 +15,26 @@ akaze = cv2.AKAZE_create()
 kp1, des1 = akaze.detectAndCompute(a_gray, None)
 kp2, des2 = akaze.detectAndCompute(b_gray, None)
 
-# 総当たりマッチング(Brute-force matcher)で特徴量マッチング
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-matches = bf.match(des1, des2)
+# Brute-force matcher生成
+bf = cv2.BFMatcher()
+
+# 特徴量ベクトル同士をBrute-Force＆KNNでマッチング
+matches = bf.knnMatch(des1, des2, k=2)
+
+# データを間引きする
+ratio = 0.75
+good = []
+for m, n in matches:
+    if m.distance < ratio * n.distance:
+        print(m.distance)
+        good.append([m])
+
+
+# matches = bf.match(des1, des2)
 # 特徴点同士の距離でソート
-matches = sorted(matches, key=lambda x: x.distance)
+# matches = sorted(matches, key=lambda x: x.distance)
 # 一番近い距離を抽出
-print(matches[0].distance)
+# print(matches[0].distance)
 
 # 上位10個の合計距離とかで判断してもいいかも
 # matches[:10]
