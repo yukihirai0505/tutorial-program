a09fbb3732c73adcb3190531f754c016fd9683de
Yuki Hirai <yukihirai0505@gmail.com>
2019-07-10 17:46:08 +0900

    Add slice rust

---
+++ programming/rust/race-conditions/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let refers_nothing = dangle();
+    println!("{}", refers_nothing);
+}
+
+fn dangle() -> String {
+    let s = String::from("hello");
+
+    s
+}
---
+++ programming/rust/slices/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let s = String::from("Hello, World!");
+    let hello = &s[0..5];
+
+    println!("hello contains = {}", hello)
+}
