7675d498146254093ee94947a83713a0c40e1934
Yuki Hirai <yukihirai0505@gmail.com>
2019-07-10 20:20:45 +0900

    Add rectangle rust

---
+++ programming/rust/rectangles/src/main.rs
@@ -0,0 +1,18 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("Area is {}", rect1.area());
+}
